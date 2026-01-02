0243810d76e2be547e6b0ae51d6d9d779dc45b3b
RQchen <chenruoqi950@gmail.com>
2026-01-02 15:20:28 +0800

    v3.跳轉勉強完成

--- js/Header.js
+++ js/Header.js
@@ -1,7 +1,8 @@
 document.getElementById('header').innerHTML = `
     <nav>
-        <img src = "image/Mutsumi.PNG" alt = "照片" width = "100" height = "100">
-        <a href= "index.html">首頁</a>
+        <img src = "../data/image/Mutsumi.PNG" alt = "照片" width = "100" height = "100">
+        <a href= "../html/school_timetable.html">課表</a>
+        <a href= "../index.html">首頁</a>
         <a href= "https://www.pu.edu.tw/">靜宜大學</a>
     </nav>
 `;
---
+++ js/image_run.js
@@ -0,0 +1,3 @@
+document.getElementById('run').innerHTML = `
+    <img src="data/image/image01.jpeg" width="2000" height="1200">
+`;
