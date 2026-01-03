ee4c623b7079ca7fb8687b934ead46f4b75dcf65
RQchen <chenruoqi950@gmail.com>
2026-01-03 14:49:18 +0800

    v4.差不多完蛋啦

--- js/Footer.js
+++ js/Footer.js
@@ -1,1 +1,9 @@
-document.getElementById('footder').innerHTML = '測試測試';
+document.getElementById('footder').innerHTML = `
+    <nav>
+        <p>
+            <img src = "../data/image/kotone.png"  width = "100" height = "100">
+            <br>資管2B
+            <br> 陳若綺
+        </p>
+    </nav>
+`;
--- js/image_run.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('run').innerHTML = `
-    <img src="data/image/image01.jpeg" width="2000" height="1200">
-`;
