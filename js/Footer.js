1c83c2a1beff64ee69af4a8a580b5ccb89c73d6f
RQchen <chenruoqi950@example.com>
2026-01-11 15:14:22 +0800

    v8.猜拳第一版本

--- js/Footer.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('footder').innerHTML = `
-    <nav>
-        <p>
-            <img src = "../data/image/kotone.png"  width = "100" height = "100">
-            <br>資管2B
-            <br> 陳若綺
-        </p>
-    </nav>
-`;
--- js/Header.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('header').innerHTML = `
-    <nav>
-        <img src = "../data/image/Mutsumi.PNG" alt = "照片" width = "100" height = "100">
-        <a href= "../html/school_timetable.html">課表</a>
-        <a href= "../html/game.html">遊戲</a>
-        <a href= "../index.html">首頁</a>
-        <a href= "https://www.pu.edu.tw/">靜宜大學</a>
-    </nav>
-`;
